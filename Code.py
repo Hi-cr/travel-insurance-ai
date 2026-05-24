b7f83f56f4d214d714f7320b3c35f3c5f5092254
Hi-cr <inch9688@gmail.com>
2026-05-24 17:24:43 +0800

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,39 +1,73 @@
-!pip install -q \ langchain \
-langchain-community \ langchain-google-genai \ faiss-cpu \
+!pip install -q langchain \
+langchain-community \ 
+langchain-google-genai \ 
+faiss-cpu \
 pypdf \
 gradio \
 google-generativeai
 
+from google.colab import files
+uploaded = files.upload ()
+
+from langchain_community.document_loaders import TextLoader
+
+loader = TextLoader ("insurance.txt",encoding="utf-8")
+documents = loader.load ( )
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+splitter = RecursiveCharacterTextSplitter(
+  chunk_size=500, 
+  chunk_overlap=50
+)
+
+docs = splitter.split_documents (documents)
+
 !pip install sentence-transformers
 
-from langchain_community embeddings import HuggingFaceEmbeddings from langchain_community vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings 
+from langchain_community.vectorstores import FAISS
+
 embeddings = HuggingFaceEmbeddings (
-model_name="sentence-transformers/all-MiniLM-L6-v2"
+  model_name="sentence-transformers/all-MiniLM-L6-v2"
+）
+
 db = FAISS. from_documents (docs, embeddings)
 
 !pip install transformers accelerate
 
 from transformers import pipeline
 qa_pipeline = pipeline(
-"text-generation", model="Qwen/Qwen2-1.5B-Instruct", device_map="auto"
+  "text-generation",
+  model="Qwen/Qwen2-1. 5B-Instruct" , 
+  device_map="auto"
 )
 
-def ask(q) :
-matched_docs = db. similarity_search (q, k=3)
-context = "\n". join([
-doc. page_content
-for doc in matched_docs
-1)
-prompt = f"wn
+def ask(q):
+  
+  matched_docs = db. similarity_search(q, k=3)
+  
+  context = "\n" join([
+    doc. page_content
+    for doc in matched_docs
+    ］）
+  
+    prompt = f"""
 根據以下旅平險條款回答問題。
 條款內容：
-{context,
+{context}
+
 問題：
-{a}
+{q}
+
 請用繁體中文回答。
-1111
+"""
+  
 result = qa_pipeline(
-prompt, max_new_tokens=200
+    prompt, 
+    max_new_tokens=200
+    )
+
 return result [0] ["generated_text"]
 
-print （ask（"班機延誤可以得到幾元補償？"））
+print（ask（"如何得知保險期間的始日與終日“））
+
